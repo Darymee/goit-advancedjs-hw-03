d5a9382b2fff50c2ebd38256a33466cc2d599214
Darymee <danieledaniele3005@gmail.com>
2025-10-24 20:56:06 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,8 +1,6 @@
 const API_BASE = 'https://pixabay.com/api/';
 const API_KEY = '29734791-3fd561d0afce25ff9315d455c';
 
-console.log('Pixabay key:', import.meta.env);
-
 export function fetchPixabayImages(query) {
   const params = new URLSearchParams({
     key: API_KEY,
@@ -15,7 +13,6 @@ export function fetchPixabayImages(query) {
   const url = `${API_BASE}?${params.toString()}`;
 
   return fetch(url).then(res => {
-    console.log(res);
     if (!res.ok) throw new Error(`HTTP ${res.status}`);
     return res.json();
   });
